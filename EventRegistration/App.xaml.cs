9dbcd04ff41486fcc16aa31f427c6d36bc3c7bfe
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-05-27 17:28:07 -0300

    Adicionar arquivos de projeto.

---
+++ EventRegistration/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EventRegistration
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EventRegistration/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EventRegistration
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
