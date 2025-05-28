e7dce72acd3a5de3a159a39a0885d4077ea55066
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-05-27 22:49:50 -0300

    feat(add): Event class refactoring and UI improvements
    - Removed redundant code and reorganized methods in the `Event` class, including new methods for calculating total price and formatting days.
    - Updated `DatePicker` in `ContractedEvent.xaml` to "dd/mm/yyyy" format.
    - Added checks in `ContractedEvent.xaml.cs` to ensure application integrity and event instance creation.
    - Improved layout structure in `EventHiring.xaml` with data binding to display event information.
    - Implemented event handling methods in `EventHiring.xaml.cs` for user feedback and navigation.

--- EventRegistration/View/ContractedEvent.xaml.cs
+++ EventRegistration/View/ContractedEvent.xaml.cs
@@ -1,18 +1,43 @@
 namespace EventRegistration.View;
+using EventRegistration.Model;
 
 public partial class ContractedEvent : ContentPage
 {
+
+    App appPropriety;
     public ContractedEvent()
     {
         InitializeComponent();
+        if (Application.Current == null) //Verifica se a instancia da aplicação atual é nula.
+        {
+            throw new Exception("Erro Aplicação nula");
+        }
+        appPropriety = (App)Application.Current; //Atribui a instancia da aplicação atual a uma variavel do tipo App.
+
+        start_data_event.MinimumDate = DateTime.Now; //Define a data minima do evento como a data atual.
+        start_data_event.MaximumDate = DateTime.Now.AddYears(1); //Define a data maxima do evento como a data atual mais um ano.
+
+        end_data_event.MinimumDate = start_data_event.Date.AddDays(1); //Define a data minima do evento como a data de inicio mais um dia.
+        end_data_event.MaximumDate = start_data_event.Date.AddMonths(6);//Define a data maxima do evento como a data de inicio mais seis meses.
     }
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
         try
         {
-            await Navigation.PushAsync(new EventHiring());
-
+#pragma warning disable CS8604 // Possível argumento de referência nula,já tratado na classe.
+            Event evento = new Event(campo_nome_evento.Text,
+                start_data_event.Date,
+                end_data_event.Date,
+                int.Parse(numberOfAttendees.Text),
+                location.Text,
+                45,
+                15);
+            await Navigation.PushAsync(new EventHiring()
+            {
+                BindingContext = evento // Define o contexto de vinculação da página de contratação do evento como o evento criado.
+            });
+            Navigation.RemovePage(this);
         }
         catch (Exception ex)
         {
--- EventRegistration/View/EventHiring.xaml.cs
+++ EventRegistration/View/EventHiring.xaml.cs
@@ -2,8 +2,20 @@ namespace EventRegistration.View;
 
 public partial class EventHiring : ContentPage
 {
-	public EventHiring()
-	{
-		InitializeComponent();
-	}
+    public EventHiring()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Parabens por contratar o evento", "O evento foi contratado com sucesso!", "OK");
+
+        Navigation.PushAsync(new ContractedEvent());
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ContractedEvent()); // Navigate back to the previous page
+    }
 }
